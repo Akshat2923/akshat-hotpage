5bad96408be35d142717f678bb0ac5405c0d5357
Akshat2923 <akshat.saladi@gmail.com>
2024-07-25 03:14:19 -0400

    about cards

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,7 +8,14 @@ import { Terminal } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 import * as React from "react";
 import Autoplay from "embla-carousel-autoplay";
-import { Card, CardContent } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+  CardHeader,
+  CardFooter,
+} from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
@@ -16,69 +23,189 @@ import {
 } from "@/components/ui/carousel";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import computer from "@/assets/images/comp.png";
+import think from "@/assets/images/think.png";
+import ponder from "@/assets/images/ponder.png";
 import osu from "@/assets/images/Ohio State Logo.png";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 export default function Page() {
   const plugin = React.useRef(
     Autoplay({ delay: 2000, stopOnInteraction: false }),
   );
+  const aboutPlugin = React.useRef(
+    Autoplay({ delay: 2000, stopOnInteraction: true }),
+  );
 
   return (
     <section className="space-y-6">
-      <H1>About Me</H1>
       <section className="space-y-3">
-        <Alert className="shadow-md">
-          <Terminal className="h-4 w-4" />
-          <AlertTitle>Who am I?</AlertTitle>
-          <AlertDescription>
-            <p>
-              My name is Akshat Saladi and I am a fourth-year student at{" "}
-              <strong>
-                <a
-                  target="_blank"
-                  href="https://cse.osu.edu/"
-                  className="flex items-center gap-2 hover:text-primary hover:underline"
-                >
-                  The Ohio State University studying Computer science and
-                  Engineering
-                  <Image
-                    src={osu}
-                    alt="a picture of the AEP logo"
-                    width={20}
-                    height={20}
-                  />
-                .
-                </a>
-              </strong>
-               I am very passionate about{" "}
-              <strong>front-end development/mobile development. </strong>I am
-              eager to learn more from others around me and help others along
-              the way!
-            </p>
-          </AlertDescription>
-        </Alert>
-        <Alert className="shadow-md">
-          <Terminal className="h-4 w-4" />
-          <AlertTitle>What drives me?</AlertTitle>
-          <AlertDescription>
-            <p>
-              One thing that you won&apos;t see on my resume is my ability to
-              help other people in need. Especially in the context and in the
-              world of tech when you are given the opportunity and the gift of
-              working on some exciting and innovative projects it makes it even
-              more rewarding when a feature might be able to directly help
-              someone struggling with something in their daily life. My backbone
-              purpose behind performing technical projects whether it&apos;s
-              individual or in the workplace is{" "}
-              <strong>
-                &quot;How can I create something that&apos;ll streamline or
-                solve someone&apos;s problems throughout their normal
-                days?&quot;
-              </strong>{" "}
-              I believe that is a great definition of a software engineer at the
-              end of the day.
-            </p>
-          </AlertDescription>
-        </Alert>
+        <div className="grid grid-cols-2 gap-4 p-4 sm:grid-cols-3 md:grid-cols-4 lg:h-screen lg:grid-cols-4 lg:grid-rows-4">
+          <Card className="col-span-1 flex sm:col-span-1 lg:col-span-1 lg:row-span-1">
+            <CardHeader>
+              <CardTitle>School</CardTitle>
+              <CardDescription>
+                <Image
+                  src={osu}
+                  alt="a picture of a ponder"
+                  width={300}
+                  height={300}
+                />
+              </CardDescription>
+            </CardHeader>
+          </Card>
+          <Card className="col-span-1 flex sm:col-span-1 lg:col-span-1 lg:row-span-1">
+            <CardHeader>
+              <CardTitle>Major</CardTitle>
+              <CardDescription>
+                I am currently in my final year of pursuing a{" "}
+                <strong>B.S. in Computer Science and Engineering.</strong>
+              </CardDescription>
+            </CardHeader>
+          </Card>
+          <Card className="col-span-2 sm:col-span-1 md:col-span-2 lg:col-span-2 lg:row-span-1">
+            <CardHeader>
+              <CardTitle>Passion</CardTitle>
+              <CardDescription>
+                I am very passionate about{" "}
+                <strong>front-end development/mobile development. </strong>I am
+                eager to learn more from others around me and help others along
+                the way!
+              </CardDescription>
+            </CardHeader>
+          </Card>
+          <Card className="sm:col-span- col-span-2 md:col-span-2 lg:col-span-1 lg:row-span-2">
+            <CardHeader>
+              <CardTitle>Courses</CardTitle>
+              
+            </CardHeader>
+            <CardContent>
+              <div className="flex flex-wrap gap-2">
+              <Badge>DS & Algos</Badge>
+                  <Badge>Databases</Badge>
+                  <Badge>Web Apps</Badge>
+                  <Badge>Networking</Badge>
+                  <Badge>Operating Sys</Badge>
+                  <Badge>Computer Org</Badge>
+                  <Badge>Software Dev & Design</Badge>
+                  <Badge>Linear Algebra</Badge>
+                  <Badge>Electronics</Badge>
+                  <Badge>Statistics</Badge>
+              </div>
+            </CardContent>
+          </Card>
+          <Card className="col-span-1 sm:col-span-1 lg:col-span-1 lg:row-span-1">
+            <CardHeader>
+              <CardTitle>Free Time?</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="flex flex-wrap gap-2">
+                <Badge>Learn new skills</Badge>
+                <Badge>Working out</Badge>
+                <Badge>Basketball</Badge>
+              </div>
+            </CardContent>
+          </Card>
+          <Card className="col-span-1 sm:col-span-1 lg:col-span-1 lg:row-span-1">
+            <CardHeader>
+              <CardTitle>Favorite language?</CardTitle>
+              <CardDescription>
+                Currently I am really enjoying <strong>TypeScript</strong>. I
+                much prefer its type safety over js.
+              </CardDescription>
+            </CardHeader>
+          </Card>
+          <Card className="order-first col-span-2 bg-transparent sm:col-span-3 md:col-span-4 lg:order-none lg:col-span-2 lg:col-start-2 lg:row-span-2 lg:row-start-2">
+            <CardHeader>
+              <CardTitle className="flex flex-col items-center">
+                <Image
+                  src={ponder}
+                  alt="a picture of a ponder"
+                  width={300}
+                  height={300}
+                />
+                Get to Know Me!
+              </CardTitle>
+            </CardHeader>
+          </Card>
+          <Card className="col-span-2 max-h-96 overflow-y-auto sm:col-span-2 md:col-span-2 lg:col-span-2 lg:row-span-1">
+            <CardHeader>
+              <CardTitle>
+                <Accordion type="single" collapsible className="w-full">
+                  <AccordionItem value="item-1">
+                    <AccordionTrigger>What Drives Me?</AccordionTrigger>
+                    <AccordionContent>
+                      One thing that you won&apos;t see on my resume is my
+                      ability to help other people in need. Especially in the
+                      context and in the world of tech when you are given the
+                      opportunity and the gift of working on some exciting and
+                      innovative projects it makes it even more rewarding when a
+                      feature might be able to directly help someone struggling
+                      with something in their daily life. My backbone purpose
+                      behind performing technical projects whether it&apos;s
+                      individual or in the workplace is{" "}
+                      <strong>
+                        &quot;How can I create something that&apos;ll streamline
+                        or solve someone&apos;s problems throughout their normal
+                        days?&quot;
+                      </strong>{" "}
+                      I believe that is a great definition of a software
+                      engineer at the end of the day.
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
+                <Image
+                  src={think}
+                  alt="a picture of a thinking"
+                  width={100}
+                  height={100}
+                  className="mx-auto"
+                />
+              </CardTitle>
+            </CardHeader>
+          </Card>
+          <Card className="col-span-2 max-h-96 overflow-y-auto sm:col-span-2 md:col-span-2 lg:col-span-2 lg:row-span-1">
+            <CardHeader>
+              <CardTitle>
+                <Accordion type="single" collapsible className="w-full">
+                  <AccordionItem value="item-1">
+                    <AccordionTrigger>Why Computer Science?</AccordionTrigger>
+                    <AccordionContent>
+                      Computer Science is a beautiful degree because of its
+                      modularity and impact. This field allows people to make
+                      small changes in software that can affect people on a
+                      broader scale. Every year since 6th grade, I would always
+                      look forward to each of Apple's Worldwide Developers
+                      Conference (WWDC) and product launches live, ecstatic to
+                      hear about the newest features coming to the next iOS
+                      because at the end of the day this affects people on a
+                      day-to-day basis. It was at WWDC that I was first
+                      introduced to Swift and XCode, sparking my interest in the
+                      field, also a little inspiration for the styling of this
+                      page :). However nothing is easy in life. The challenges
+                      that I've faced throughout my undergrad have been nothing
+                      I could've imagined. But I believe that this adversity
+                      will make you the best of people in the world, and my
+                      ability to pursue a STEM degree alongside these hardships
+                      will better me as a person.
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
+                <Image
+                  src={computer}
+                  alt="a picture of a thinking"
+                  width={100}
+                  height={100}
+                  className="mx-auto"
+                />
+              </CardTitle>
+            </CardHeader>
+          </Card>
+        </div>
       </section>
       <hr className="border-muted" />
       <section className="space-y-3">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import pic from "@/assets/images/pic.jpg";
 import { H2 } from "@/components/ui/H2";
 import { Bot } from "lucide-react";
+import hi from "@/assets/images/hi.png";
 
 export const metadata: Metadata = {
   title: "Akshat Saladi - My Hot Page",
@@ -12,19 +13,23 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <section
-      id="home"
-      className="space-y-16 px-1 py-8"
-    >
+    <section id="home" className="space-y-16 px-1 py-8">
       <section className="grid-col-1 grid items-center gap-8 sm:grid-cols-2">
         <div className="space-y-3">
           <H1 className="text-center sm:text-start">
-            Hey, I&apos;m Akshat Saladi 👋
+            <span className="inline">Hey, I&apos;m Akshat Saladi</span>
+            <Image
+              src={hi}
+              alt="a picture of me"
+              width={50}
+              height={50}
+              className="ml-2 inline-block aspect-square rounded-full object-cover align-text-bottom"
+            />
           </H1>
           <p className="text-center sm:text-start">
-            Welcome to my Hot Page. This
-            website is a smart personal portfolio where I showcase my work and
-            skills. Click the top bar links to learn more about me!
+            Welcome to my Hot Page. This website is a smart personal portfolio
+            where I showcase my work and skills. Click the top bar links to
+            learn more about me!
           </p>
         </div>
         <div className="flex justify-center">
@@ -33,15 +38,15 @@ export default function Home() {
             alt="a photo of me"
             width={300}
             height={300}
-            className="aspect-square rounded-full border-2 object-cover shadow-md dark:border-foreground [object-position:right_10%_bottom_60%]"
+            className="aspect-square rounded-full border-2 object-cover shadow-md [object-position:right_10%_bottom_60%] dark:border-foreground"
           />
         </div>
       </section>
       <section className="space-y-3 text-center">
         <H2>...Or ask the chatbot anything</H2>
         <p>
-          Click the <Bot className="inline pb-1" /> icon in the top bar
-          to active the chatbot. You can ask it anything you want about me!
+          Click the <Bot className="inline pb-1" /> icon in the top bar to
+          active the chatbot. You can ask it anything you want about me!
         </p>
       </section>
     </section>
