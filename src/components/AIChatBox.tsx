70b2364698e4597f9d8ec596cd37f0d014da07d0
Akshat2923 <akshat.saladi@gmail.com>
2024-08-01 01:33:43 -0400

    rounded again

--- src/components/AIChatBox.tsx
+++ src/components/AIChatBox.tsx
@@ -337,7 +337,7 @@ const QuestionCard = ({ body }: { body: string }) => {
   return (
     <figure
       className={cn(
-        "relative w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
+        "relative w-64 cursor-pointer overflow-hidden rounded-2xl border p-4",
         // light styles
         "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
         // dark styles
